51bca00c95978bf0701260177256fbf51b703f23
Bonsaikiller <mistbox@hotmail.com>
2024-07-02 10:13:34 +0200

    Initial Commit

---
+++ SowAndGrow/SowAndGrowApp.swift
@@ -0,0 +1,17 @@
+//
+//  SowAndGrowApp.swift
+//  SowAndGrow
+//
+//  Created by Susanne Dvorak on 02.07.24.
+//
+
+import SwiftUI
+
+@main
+struct SowAndGrowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
